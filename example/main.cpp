bbaaf08e87b93baa329ef368261b0dab750d534c
David Vandal <dsv101@gmail.com>
2019-04-07 14:11:07 -0400

    new example

--- example/main.cpp
+++ example/main.cpp
@@ -30,9 +30,281 @@ c 2/4\n\
 c 3/4\n\
 ";
 
+std::string nd2 = "\
+bpm 169\n\
+sud 2\n\
+\n\
+m\n\
+\n\
+m\n\
+\n\
+t 4/8\n\
+c 5/8\n\
+t 6/8\n\
+c 7/8\n\
+\n\
+m\n\
+qh\n\
+qhe 1/2\n\
+ch 1/2\n\
+\n\
+m\n\
+che\n\
+t\n\
+q 1/4\n\
+c 2/4\n\
+t 3/4\n\
+\n\
+m\n\
+ch\n\
+che 1/2\n\
+q 1/2\n\
+l 7/8\n\
+\n\
+m\n\
+r 1/8\n\
+l 3/8\n\
+r 5/8\n\
+l 7/8\n\
+\n\
+m\n\
+q 1/4\n\
+c 1/4\n\
+t 2/4\n\
+c 2/4\n\
+q 3/4\n\
+t 3/4\n\
+\n\
+m\n\
+ch\n\
+t 1/4\n\
+q 2/4\n\
+che 3/4\n\
+q 3/4\n\
+\n\
+m\n\
+qh\n\
+qhe 1/2\n\
+r 7/8\n\
+\n\
+m\n\
+l 1/8\n\
+r 3/8\n\
+l 5/8\n\
+\n\
+m\n\
+q\n\
+c\n\
+q 1/4\n\
+c 1/4\n\
+q 2/4\n\
+c 2/4\n\
+q 3/4\n\
+c 3/4\n\
+\n\
+m\n\
+t\n\
+c\n\
+t 2/8\n\
+c 2/8\n\
+c 3/8\n\
+q 4/8\n\
+q 5/8\n\
+t 6/8\n\
+t 7/8\n\
+\n\
+m\n\
+q\n\
+t\n\
+q 1/4\n\
+t 1/4\n\
+q 2/4\n\
+t 2/4\n\
+q 3/4\n\
+t 3/4\n\
+\n\
+m\n\
+c\n\
+c 1/8\n\
+t 1/8\n\
+c 3/8\n\
+q 3/8\n\
+c 5/8\n\
+t 7/8\n\
+\n\
+m\n\
+qh\n\
+l 2/4\n\
+r 3/4\n\
+\n\
+m\n\
+qhe\n\
+ch\n\
+r 2/4\n\
+l 3/4\n\
+\n\
+m\n\
+che\n\
+th\n\
+l 2/4\n\
+r 3/4\n\
+\n\
+m\n\
+the\n\
+q 1/8\n\
+c 1/8\n\
+q 2/8\n\
+c 2/8\n\
+q 3/8\n\
+c 3/8\n\
+q 4/8\n\
+c 4/8\n\
+q 5/8\n\
+c 5/8\n\
+q 6/8\n\
+c 7/8\n\
+q 7/8\n\
+\n\
+m\n\
+t\n\
+t 1/4\n\
+t 2/4\n\
+t 3/4\n\
+c 7/8\n\
+\n\
+m\n\
+qh\n\
+c 1/4\n\
+t 2/4\n\
+q 3/4\n\
+\n\
+m\n\
+t\n\
+c 1/4\n\
+t 2/4\n\
+c 3/4\n\
+qhe 3/4\n\
+\n\
+m\n\
+ch\n\
+t 1/4\n\
+q 2/4\n\
+c 3/4\n\
+\n\
+m\n\
+q\n\
+t 1/4\n\
+q 2/4\n\
+t 3/4\n\
+che 3/4\n\
+\n\
+m\n\
+th\n\
+q 1/4\n\
+c 2/4\n\
+t 3/4\n\
+\n\
+m\n\
+c\n\
+q 1/4\n\
+c 2/4\n\
+q 3/4\n\
+the 3/4\n\
+\n\
+m\n\
+qh\n\
+c 1/4\n\
+q 2/4\n\
+t 3/4\n\
+\n\
+m\n\
+qhe\n\
+t\n\
+c 2/8\n\
+t 3/8\n\
+q 4/8\n\
+t 5/8\n\
+c 6/8\n\
+r 7/8\n\
+\n\
+m\n\
+q 1/4\n\
+c 2/4\n\
+t 3/4\n\
+l 7/8\n\
+\n\
+m\n\
+t 1/4\n\
+t 2/4\n\
+t 3/4\n\
+\n\
+m\n\
+q\n\
+q 1/4\n\
+c 4/8\n\
+t 5/8\n\
+c 6/8\n\
+t 7/8\n\
+q 7/8\n\
+\n\
+m\n\
+l 3/8\n\
+r 5/8\n\
+l 7/8\n\
+\n\
+m\n\
+q 4/8\n\
+c 5/8\n\
+th 7/8\n\
+\n\
+m\n\
+the 3/8\n\
+q 3/8\n\
+c 7/8\n\
+\n\
+m\n\
+t 4/8\n\
+t 5/8\n\
+q 7/8\n\
+\n\
+m\n\
+q 3/8\n\
+c 3/8\n\
+\n\
+m\n\
+q\n\
+t 1/8\n\
+q 2/8\n\
+t 3/8\n\
+q 4/8\n\
+t 5/8\n\
+q 6/8\n\
+t 7/8\n\
+\n\
+m\n\
+c 5/16\n\
+t 7/8\n\
+\n\
+m\n\
+t 1/4\n\
+c 9/16\n\
+q 7/8\n\
+\n\
+m\n\
+q 5/16\n\
+\n\
+m\n\
+qh\n\
+ch\n\
+qhe 1/4\n\
+che 1/4\n\
+\
+";
+
 int main()
 {
 	idc::parser::Song song;
+	idc::parser::Song song2;
 
 	bool loaded = song.load_data(nd);
 
@@ -50,6 +322,13 @@ int main()
 
 		for (std::vector<idc::parser::Song::SeekResult>::iterator it = results.begin(); it != results.end(); ++it)
 			std::cout << (*it).to_string() << std::endl;
+	}
+
+	if (!song2.load_data(nd2))
+		std::cout << song2.get_error() <<std::endl;
+	else
+	{
+		std::cout << song2.to_string() << std::endl << std::endl;
 	}
 
 	return 0;
